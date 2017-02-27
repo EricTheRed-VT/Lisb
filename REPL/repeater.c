721db479a5270e1676ad4d6b1db84fc2b6892b02
Eric <ericrainvillevt@gmail.com>
2017-02-27 16:55:57 -0500

    fancy repeater

--- REPL/repeater.c
+++ REPL/repeater.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input */
-static char input[2048];
+/* If compiling on Windows, use these */
+#ifdef _WIN32
+#include <string.h>
 
-int main(int argc, char** argv)
-{
+static char buffer[2048];
+
+/* fake readline func */
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc (strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add-history func */
+void add_history(char* unused) {}
+
+/* If not on Windows, include editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
+
+int main(int argc, char** argv) {
+
   /* Print Version and Exit Info */
   puts("Lisb Version 0.0.1");
   puts("Press Ctrl+C to Exit\n");
 
   /* Infinite loop */
   while (1) {
-    /* Output prompt */
-    fputs("lisb> ", stdout);
 
-    /* Read a line of input */
-    fgets(input, 2048, stdin);
+    /* Output prompt and get input */
+    char* input = readline("lisb> ");
+
+    /* add input to history */
+    add_history(input);
 
     /* Echo input back */
-    printf("You said: %s", input);
+    printf("You said: %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
+
   }
 
   return 0;
