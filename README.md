f71e7660b82b8be244fa2bb3106d1be0aa7541e1
Eric <ericrainvillevt@gmail.com>
2017-03-05 17:39:48 -0500

    add printing from lval tree

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Lisb
 
 A compiler/etc for a Lisp-like language. This serves as an exercise to learn C, study compiler implementation and theory, and become familiar with Lisp.
+
+(Lisb uses variable sized arrays to represent S-Expressions, as opposed to the usual linked lists.)
--- s-expressions.c
+++ s-expressions.c
@@ -79,6 +79,14 @@ lval* lval_sexpr(void) {
   return v;
 }
 
+/* Add an lval 'x' as a child to a sexpr 'v' */
+lval* lval_add(lval* v, lval* x) {
+  v->count++;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  v->cell[v->count-1] = x;
+  return v;
+}
+
 /* Delete an lval, and all its pointers/data */
 void lval_del(laval* v) {
   switch (v-> type) {
@@ -96,21 +104,67 @@ void lval_del(laval* v) {
   free(v);
 }
 
+/* Create a number lval from an AST leaf */
+lval* lval_read_num(mpc_ast_t* t) {
+  errno = 0;
+  long x = strol(t->contents, NULL, 10);
+  return errno != ERANGE
+    ? lval_num(x)
+    : lval_err("Invalid Number");
+}
+
+/* create lval tree from AST */
+lval* lval_read(mpc_ast_t* t) {
+  /* if leaf, assign correct lval type */
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  /* if root or sexpr create empty list */
+  lval* x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+
+  /* add valid children */
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+    if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+
+  return x;
+}
+
+/* print an sexpr */
+void lval_expr_print(lval* v, char open, char close) {
+  putchar(open);
+
+  for (int i = 0; i < v->count; i++)
+  {
+    lval_print(v->cell[i]);
+    if (i != (v->count-1))
+    {
+      putchar(' ');
+    }
+  }
+
+  putchar(close);
+}
+
 /* print an lval */
-void lval_print(lval v) {
-  switch (v.type) {
-    case LVAL_NUM: printf("%li", v.num); break;
-    case LVAL_ERR:
-      switch (v.err) {
-        case LERR_DIV_ZERO: printf("Error: Division by Zero"); break;
-        case LERR_BAD_OP: printf("Error: Invalid Operator"); break;
-        case LERR_BAD_NUM: printf("Error: Invalid Number"); break;
-      }
+void lval_print(lval* v) {
+  switch (v->type) {
+    case LVAL_NUM: printf("%li", v->num); break;
+    case LVAL_ERR: printf("Error: %s", v->err); break;
+    case LVAL_SYM: printf("%s", v->sym); break;
+    case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
   }
 }
 
 /* print an lval and newline */
-void lval_println(lval v) { lval_print(v); putchar('\n'); }
+void lval_println(lval* v) { lval_print(v); putchar('\n'); }
 
 /************************* EVAL FUNCS *************************/
 
